5e864991afe733736bbbbfbf6d1a7bc8ab7ba6e7
Adam Carballido <carballido.adam@yahoo.com>
2017-09-30 00:45:52 -0400

    Added rotate function to door and added other assets

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -20,7 +20,10 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos); 
 	
 }
 
